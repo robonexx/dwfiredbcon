9dc66a416dc1bc499c38cd0d6a180bf2cb9c56cd
robonexx <robertwagar@gmail.com>
2021-08-12 16:04:49 +0200

    added function to write a article and push i to the db

--- app.js
+++ app.js
@@ -2,18 +2,21 @@ const articlesList = document.querySelector('.articlesList')
 const titlePost = document.querySelector('.writeTitle')
 const authorPost = document.querySelector('.writeAuthor')
 const textPost = document.querySelector('.writeText')
+const form = document.querySelector('form')
 
 const addArticle = (article) => {
 
-    let dateTime = article["created.at"].toDate()
+   /*  const dateTime = article["created.at"].toDate() */
     
     let html = `
     <div class="article">
                 <h2 class="articleTitle">Title: <b>${article.title}</b> Author: <b>${article.author}</b></h2>
                 <div class="articleBody">${article.body}</div>
-               <span class="time">${dateTime}</span>
+                <button class="deleteBtn">Delete</button>
+               
     </div>
     `;
+    /* <span class="time">${dateTime}</span> */
 
     articlesList.innerHTML += html
 }
@@ -23,12 +26,30 @@ db.collection('articles').get().then((snapshot) => {
     
     snapshot.docs.forEach(doc => {
         addArticle(doc.data());
-       /*  console.log(doc.data()) */
+        console.log(doc.data()) 
     })
 
 }).catch(err => {
     console.log(err)
 })
 
-/* created: firebase.database.ServerValue.TIMESTAMP */
+ 
+form.addEventListener('submit', e => {
+    e.preventDefault()
+
+    const article = {
+        title: form.title.value,
+        author: form.author.value,
+        created: firebase.firestore.FieldValue.serverTimestamp(),
+        body: form.textBody.value
+    }; 
+    db.collection('articles').add(article).then(() => {
+        console.log('article added to db')
+    }).catch(err => {
+        console.log(err)
+    })
+
+    form.reset()
+}) 
   
+  //delete
