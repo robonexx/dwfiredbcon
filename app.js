28711989a0c274e529803755bb7e96d4e70d5be4
robonexx <robertwagar@gmail.com>
2021-08-12 12:43:17 +0200

    added template for the retireved data

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
+const articlesList = document.querySelector('.articlesList')
+const titlePost = document.querySelector('.writeTitle')
+const authorPost = document.querySelector('.writeAuthor')
+const textPost = document.querySelector('.writeText')
+
+const addArticle = (article) => {
+
+    let dateTime = article["created.at"].toDate()
+    
+    let html = `
+    <div class="article">
+                <h2 class="articleTitle">Title: <b>${article.title}</b> Author: <b>${article.author}</b></h2>
+                <div class="articleBody">${article.body}</div>
+               <span class="time">${dateTime}</span>
+    </div>
+    `;
+
+    articlesList.innerHTML += html
+}
 
 db.collection('articles').get().then((snapshot) => {
       // the data 
     
-    snapshot.docs.forEach((doc => {
-    console.log(doc.data())
-    }))
+    snapshot.docs.forEach(doc => {
+        addArticle(doc.data());
+       /*  console.log(doc.data()) */
+    })
+
 }).catch(err => {
     console.log(err)
 })
+
+/* created: firebase.database.ServerValue.TIMESTAMP */
   
