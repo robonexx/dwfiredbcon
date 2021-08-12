4e5cd4e5cb87c902ce7c01cee274fcda5269bb16
robonexx <robertwagar@gmail.com>
2021-08-12 11:39:21 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+/* var firebaseConfig = {
+    apiKey: "AIzaSyDG3kDYD0dVP2ncOh8M8KF_BeM9ILpTSgA",
+    authDomain: "dailywriting-37980.firebaseapp.com",
+    projectId: "dailywriting-37980",
+    storageBucket: "dailywriting-37980.appspot.com",
+    messagingSenderId: "372738870868",
+    appId: "1:372738870868:web:2b57c426758cc6db0ee7c1"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig); */
