137b24c24326fbbca2282f5168fbeca409f31f1e
robonexx <robertwagar@gmail.com>
2021-08-12 11:55:02 +0200

    oops

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-/* var firebaseConfig = {
-    apiKey: "AIzaSyDG3kDYD0dVP2ncOh8M8KF_BeM9ILpTSgA",
-    authDomain: "dailywriting-37980.firebaseapp.com",
-    projectId: "dailywriting-37980",
-    storageBucket: "dailywriting-37980.appspot.com",
-    messagingSenderId: "372738870868",
-    appId: "1:372738870868:web:2b57c426758cc6db0ee7c1"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig); */
+
+db.collection('articles').get().then((snapshot) => {
+      // the data 
+    
+    snapshot.docs.forEach((doc => {
+    console.log(doc.data())
+    }))
+}).catch(err => {
+    console.log(err)
+})
+  
